ee14b64b4641ab2a02b5738e4b224c1897e0529a
MrFlamadak <thecool4a@gmail.com>
2023-04-15 06:19:19 +0200

    Adding multivareuler.m file & modifying task1.m
    multivareuler.m is a function that solves the differential equations of
    task1. task1.m is a program that calls on the function and plots the
    desired graph.

--- Task1/task1.m
+++ Task1/task1.m
@@ -1,9 +1,8 @@
 % SF1546 Numerical Methods, Basic Course, Project A
 
 % begin with simplifying the equations to first order differential equations
-
-a = 1;
-b = 2;
-c = a+b;
-c
-
+format long
+[x, y] = multivareuler(0, 1.84, 15, 0.0000005, 4, 0.02);
+plot(x, y)
+y(end)
+x(end)
