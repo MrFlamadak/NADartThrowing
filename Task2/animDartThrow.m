d003197bebf15a5269926d8dcf65d1c9cf2aec09
Mostafa Aziz Zuher <thecool4a@gmail.com>
2023-04-23 10:27:04 +0200

    separated animation and calculation

---
+++ Task2/animDartThrow.m
@@ -0,0 +1,19 @@
+% SF1546 Numerical Methods, Basic course, Project A
+% animDartThrow: Animates the dart throw with respect to the throwing angle
+function animDartThrow()
+close(1)
+A = zeros(601, 2);
+index = 1;
+for x = 0:0.1:85
+    A(index, 2) = bullsEyeDistance(h, x);
+    A(index, 1) = x;
+    index = index + 1;
+end
+plot(A(:, 1), A(:, 2))
+end
+% this finds both solutions to task 2! visually.
+
+% start: 5, 4 root = 4.199249961710662
+
+%[diff, yend, x, y] = multivareuler2(4);
+%guess = fzero(@multivareuler2, guess, options);
--- Task2/task2.m
+++ Task2/task2.m
@@ -17,23 +17,9 @@ a = 0;
 
 %tn = 8.1001e-08 => y2 har 7 decimalers noggranhet
 % secant time
-svar = secantA(h, a, 1e-6)
-noll = bullsEyeDistance(h, svar)
+rot1 = secantA(h, a, 1e-4)
+noll1 = bullsEyeDistance(h, rot1)
 
-% animate the dart throw with respect to the throwing angle
-close(1)
-A = zeros(601, 2)
-index = 1;
-for x = 0:0.1:85
-    A(index, 2) = bullsEyeDistance(h, x);
-    A(index, 1) = x;
-    index = index + 1;
-end
-plot(A(:, 1), A(:, 2))
-% this finds both solutions to task 2! visually.
 
-% start: 5, 4 root = 4.199249961710662
-
-%[diff, yend, x, y] = multivareuler2(4);
-%guess = fzero(@multivareuler2, guess, options);
-
+rot2 = secantA(h, 80, 1e-4)
+noll2 = bullsEyeDistance(h, rot2)
