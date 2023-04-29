769ab0381e1a41d63ca73671d5804a70b7857d8b
Roy Liu <113428371+ruisnake@users.noreply.github.com>
2023-04-29 12:05:04 +0200

    Update marginOfError1.m
    Added some spaces

--- Task1/marginOfError1.m
+++ Task1/marginOfError1.m
@@ -6,9 +6,9 @@ h = 0.000001;
 
 [x, y2] = multiVarEuler(0, 1.84, 15, h/2, 4, 0.02);
 [x, y3] = multiVarEuler(0, 1.84, 15, h/4, 4, 0.02);
-error1 = y1(end)-y2(end);
-error2 = y2(end)-y3(end);
-richardson = error1/error2
+error1 = y1(end) - y2(end);
+error2 = y2(end) - y3(end);
+richardson = error1 / error2
 
 % Svar: 1.82662358 +- 8.1*e-08     
 
