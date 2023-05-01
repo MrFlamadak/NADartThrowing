4010476e8def364d200233f74dfc3a4f40678e5b
Mostafa Aziz Zuher <115707558+MrFlamadak@users.noreply.github.com>
2023-05-01 11:47:49 +0200

    Update marginOfError3.m

--- Task3/marginOfError3.m
+++ Task3/marginOfError3.m
@@ -11,16 +11,16 @@ tol = 1e-5;
 
 % case 1
 d1 = bullsEyeDistanceV(h, root + tn)
-d2 = bullsEyeDistanceV(h/2, root + tn)
-d3 = bullsEyeDistanceV(h/4, root + tn)
+d2 = bullsEyeDistanceV(h / 2, root + tn)
+d3 = bullsEyeDistanceV(h / 4, root + tn)
 richard1 = abs(d1 - d2) / abs(d2 - d3)
 error1 = d1 - d2
 % answer: 2.2204e-16 +- 7.8427e-08
 
 % case 2
 d4 = bullsEyeDistanceV(h, root - tn)
-d5 = bullsEyeDistanceV(h/2, root - tn)
-d6 = bullsEyeDistanceV(h/4, root - tn)
+d5 = bullsEyeDistanceV(h / 2, root - tn)
+d6 = bullsEyeDistanceV(h / 4, root - tn)
 richard2 = abs(d4 - d5) / abs(d5 - d6)
 error2 = d4 - d5
 % answer: -1.2145e-13 +- 7.8427e-08
