da00ef5e07c1088e86ac3788f665b7eeca7698d1
Mostafa Aziz Zuher <115707558+MrFlamadak@users.noreply.github.com>
2023-05-01 11:46:25 +0200

    Update secantV.m

--- Task3/secantV.m
+++ Task3/secantV.m
@@ -2,14 +2,16 @@
 % secantV.m - Finds what velocity is required for hitting bullseye when
 % angle is 3 degrees
 function [root, tn] = secantV(h, v, tol)
-    vn = v+1; vnn = v;
+    vn = v + 1; vnn = v;
     f = @(x) bullsEyeDistanceV(h, x);
     tn = 1;
-    while abs(tn)>tol
-        tn = f(vnn)*(vnn-vn)/(f(vnn) - f(vn));
+    
+    while abs(tn) > tol
+        tn = f(vnn) * (vnn-vn) / (f(vnn) - f(vn));
         vn = vnn;
         vnn = abs(vnn - tn);
     end
+    
     root = vn;
     tn = abs(tn)
 
