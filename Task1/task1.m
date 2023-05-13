1a4f5980ffc4b7cc4b6f26fedc591c23ebbfe068
Mostafa Aziz Zuher <thecool4a@gmail.com>
2023-05-13 11:02:57 +0200

    Tried interpolating points

--- Task1/task1.m
+++ Task1/task1.m
@@ -4,5 +4,20 @@ format long
 h = 0.000001;
 [x, y] = multiVarEuler(0, 1.84, 15, h, 4, 0.02);
 plot(x, y)
-y = y(end)
-x = x(end)
+y(end)
+x(end)
+% linear interpolation was performed to see if there were any improvements
+% to be made, however the difference was negligible and so the
+% aforementioned results are satisfactory
+% xpoints = x(end-4:end)
+% ypoints = y(end-4:end)
+% %plot(xpoints, ypoints)
+% xspan = linspace(xpoints(1), xpoints(end));
+% k = (ypoints(3)-ypoints(2))/(xpoints(3)-xpoints(2));
+% m = ypoints(1) - k*xpoints(1);
+% yval = k.*xspan + m;
+% plot(xspan, yval)
+
+ 
+
+
