e87372745249fe25a36773c27c0c14263c4dea3d
Mostafa Aziz Zuher <thecool4a@gmail.com>
2023-05-21 14:22:37 +0200

    Fixed some compilation errors

--- Task1/marginOfError1.m
+++ Task1/marginOfError1.m
@@ -11,7 +11,6 @@ graph = @(x) constants(1) + constants(2) * (x - xArr(1)) + constants(3) * (x - x
 %x = linspace(0, 2.37);
 plot(x, graph(x))
 newt1 = graph(2.37) - 1.83
-diff = abs(withIntpol - newt1)
 
 felgrans = newtonInterpol(x(end-3:end), y(end-3:end))
 graph = @(x) felgrans(1) + felgrans(2) * (x - xArr(1)) + felgrans(3) * ((x - xArr(1)) .* (x - xArr(2))) + felgrans(4) * ((x - xArr(1)) .* (x - xArr(2)) .* (x - xArr(3)));
@@ -19,12 +18,11 @@ plot(x, graph(x))
 newt2 = graph(2.37) - 1.83
 answer = graph(2.37)
 trunk = abs(newt1 - newt2)
-pai = abs(answer - ogAnswer)
 
 % Answer with Newton's Polynomial
-% 1.826624106511459
+% 1.8266241
 % Truncation error
-% 6.024222953815439e-07
+% 6.0242e-07
 
 % Final answer
 % 1.8266241 +- 6e-07 m
