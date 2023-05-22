e1d78762fd3f4a107edc45dc16af756e20e33b99
Mostafa Aziz Zuher <thecool4a@gmail.com>
2023-05-22 14:53:03 +0200

    Corrected error margin mistakes

--- Task2/marginOfError2.m
+++ Task2/marginOfError2.m
@@ -4,46 +4,43 @@
 % Therefore having a tolerance and steplength under this would be valid
 % since it is within the eye.
 format long
-h = 1e-04;
-tol = 1e-04;
+% Standardized variables
+m = 20e-3;  
+v = 15;     
+y0 = 1.84;
+h = 1e-5;
+tol = 1e-4;
 guess1 = 0;
 guess2 = 80;
 
 [rot1, tn1] = secantA(h, guess1, tol);
-secanterror1 = tn1
+secanterror1 = abs(tn1)
 rot1
-% secanterror1 = 1.03963e-05
-% eulererror1 = 3.81303e-05
-% tot = 4.85267e-05
 
 d1 = bullsEyeDistanceA(h, rot1 + tn1);
 d2 = bullsEyeDistanceA(h/2, rot1 + tn1);
 d3 = bullsEyeDistanceA(h/4, rot1 + tn1);
 richardson1 = (d1 - d2) / (d2 - d3)
-eulererror1 = (d1) - (d2)
-svar1 = d1
-tot = eulererror1 + secanterror1
-d1 = bullsEyeDistanceA(h, rot1 - tn1);
-d2 = bullsEyeDistanceA(h/2, rot1 - tn1);
-d3 = bullsEyeDistanceA(h/4, rot1 - tn1);
-richardson2 = (d1 - d2) / (d2 - d3)
-eulererror2 = d1 - d2
-svar2 = d1
+eulererror1 = abs((d3) - (d2))
+tot1 = eulererror1 + secanterror1
 
-[rot2, tn2] = secantA(h, guess2, tol);
-secanterror2 = tn2;
+% Eulererror1 = 1.91e-06
+% SecantError1 = 6.62e-05
+% Tot1 = 6.82e-05
 
-d1 = bullsEyeDistanceA(h, rot2 - tn2);
-d2 = bullsEyeDistanceA(h/2, rot2 - tn2);
-d3 = bullsEyeDistanceA(h/4, rot2 - tn2);
+[rot2, tn2] = secantA(h, guess2, tol)
+secanterror2 = abs(tn2)
+
+d1 = bullsEyeDistanceA(h, rot2 + tn2);
+d2 = bullsEyeDistanceA(h/2, rot2 + tn2);
+d3 = bullsEyeDistanceA(h/4, rot2 + tn2);
 richardson2 = (d1 - d2) / (d2 - d3)
-eulererror2 = d1 - d2
-svar2 = d1
+eulererror2 = abs(d3 - d2)
 tot2 = eulererror2 + secanterror2
 
-% secanterror2 = 3.07021e-05
-% eulererror2 = 8.25860e-05
-% tot2 = 1.13288e-04
+% Secanterror2 = 2.63e-05
+% Eulererror2 = 2.88e-06
+% Tot2 = 2.92e-05
 
 % This is the first time we have worked with accumulative error margins and
 % so we assume the separate method errors can be combined additively.
--- Task2/task2.m
+++ Task2/task2.m
@@ -8,7 +8,7 @@ v = 15;
 y0 = 1.84;
 h = 1e-5;
 tol = 1e-4;
-h = 0.1e-4;
+
 aGuess1 = 0;
 % first root
 [root1, tn1] = secantAll(y0, v, h, aGuess1, m, tol)
@@ -26,7 +26,8 @@ k = newtonInterpol(x2, y2);
 yfunc2 = @(x) k(1) + k(2) * (x-x2(1)) + k(3) * (x-x2(1)) * (x-x2(2)) + k(4) * (x-x2(1)) * (x-x2(2)) * (x-x2(3));
 y237n2 = yfunc2(2.37);
 diff = y237-y237n2
-% 1.830019063129496
+% interpolerror = -6.88e-15 (negligible)
+%   1.830019063129496
 %   -0.050643301389363
 %   -0.012432980169140
 %    0.001398103787500
@@ -48,11 +49,11 @@ yfunc2 = @(x) k(1) + k(2) * (x-x2(1)) + k(3) * (x-x2(1)) * (x-x2(2)) + k(4) * (x
 y237n2 = yfunc2(2.37);
 diff = y237-y237n2
 % ANSWERS: 
-% (1) InitialAngle = 4.198963 +- 6.62-05
-% gives 1.8299970 +- 2.96-06 m
- 
-% (2) InitialAngle = 80.037702 +- 2.63-05
-% gives 1.829989 +- 1-05 m
+% (1) InitialAngle = 4.198963 +- 6.82e-05
+% gives 1.8299970 +- which is 2.96-06 m from 1.83
+
+% (2) InitialAngle = 80.037703 +- 2.92-05
+% gives 1.829989  which is 1-05 m from 1.83
 
 % animDartThrow(h) animates the dart's travel from 0 to 85 degrees
 % un-comment the function below to see
