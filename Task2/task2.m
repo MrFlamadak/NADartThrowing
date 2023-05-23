26950f04f890e951291f93e6043c012efbe34e9e
Mostafa Aziz Zuher <thecool4a@gmail.com>
2023-05-23 11:46:01 +0200

    small revision for clarity in presentation

--- Task2/task2.m
+++ Task2/task2.m
@@ -11,46 +11,48 @@ tol = 1e-4;
 
 aGuess1 = 0;
 % first root
-[root1, tn1] = secantAll(y0, v, h, aGuess1, m, tol)
+[root1, tn1] = secantAll(y0, v, h, aGuess1, m, tol);
 [d, xd, yd] = bullsEyeDistanceAll(y0, v, h, root1, m);
 x1 = xd(end - 2:end);
 y1 = yd(end - 2:end);
-k = newtonInterpol(x1, y1)
+k = newtonInterpol(x1, y1);
 yfunc = @(x) k(1) + k(2) * (x-x1(1)) + k(3) * (x-x1(1)) * (x-x1(2));
-y237 = yfunc(2.37)
-errormargin = yfunc(2.37) - 1.83
+y237_1 = yfunc(2.37)
 % Interpol margin of error
 x2 = xd(end - 3:end);
 y2 = yd(end - 3:end);
 k = newtonInterpol(x2, y2);
 yfunc2 = @(x) k(1) + k(2) * (x-x2(1)) + k(3) * (x-x2(1)) * (x-x2(2)) + k(4) * (x-x2(1)) * (x-x2(2)) * (x-x2(3));
 y237n2 = yfunc2(2.37);
-diff = y237-y237n2
+diff = y237-y237n2;
 % interpolerror = -6.88e-15 (negligible)
 %   1.830019063129496
 %   -0.050643301389363
 %   -0.012432980169140
 %    0.001398103787500
 aGuess2 = 80;
+
 % second root
-[root2, tn2] = secantAll(y0, v, h, aGuess2, m, tol)
+[root2, tn2] = secantAll(y0, v, h, aGuess2, m, tol);
 [d, xd, yd] = bullsEyeDistanceAll(y0, v, h, root2, m);
 x1 = xd(end - 2:end);
 y1 = yd(end - 2:end);
-k = newtonInterpol(x1, y1)
+k = newtonInterpol(x1, y1);
 yfunc = @(x) k(1) + k(2) * (x-x1(1)) + k(3) * (x-x1(1)) * (x-x1(2));
-y237 = yfunc(2.37)
-errormargin = yfunc(2.37) - 1.83
+y237_2 = yfunc(2.37)
 
-maxBullsyedistance1 = bullsEyeDistanceAll(y0, v, h, root1 + 6.82e-05, m)
-maxBullsyedistance2 = bullsEyeDistanceAll(y0, v, h, root2 + 2.92e-05, m)
+
+maxBullsyedistance1 = bullsEyeDistanceAll(y0, v, h, root1 + 6.82e-05, m);
+maxBullsyedistance2 = bullsEyeDistanceAll(y0, v, h, root2 + 2.92e-05, m);
 % Interpol margin of error
 x2 = xd(end - 3:end);
 y2 = yd(end - 3:end);
 k = newtonInterpol(x2, y2);
 yfunc2 = @(x) k(1) + k(2) * (x-x2(1)) + k(3) * (x-x2(1)) * (x-x2(2)) + k(4) * (x-x2(1)) * (x-x2(2)) * (x-x2(3));
 y237n2 = yfunc2(2.37);
-diff = y237-y237n2
+diff = y237-y237n2;
+root1
+root2
 % ANSWERS: 
 % (1) InitialAngle = 4.198963 +- 6.82e-05
 % maximumDistancefrombullseye1 = 2.30e-06
--- Task3/task3.m
+++ Task3/task3.m
@@ -5,38 +5,28 @@ format long
 h = 1e-5
 tol = 1e-4
 v = 16;
-[root, tn] = secantV(h, v, tol)
+[root, tn] = secantV(h, v, tol);
 [distance, xd, yd] = bullsEyeDistanceV(h, root);
-distance
 x1 = xd(end-2:end);
 y1 = yd(end-2:end);
 k = newtonInterpol(x1, y1);
 f = @(x) k(1) + k(2) * (x-x1(1)) + k(3) * (x-x1(1)) * (x-x1(2));
 y237 = f(2.37)
-errormargin = distance - 1.83
-root
-maxdistancefrombullseye = bullsEyeDistanceV(h, root + 5.93e-05 )
+maxdistancefrombullseye = bullsEyeDistanceV(h, root + 5.93e-05);
 
-%Interpolfelgräns
-x1 = xd(end - 2:end);
-y1 = yd(end - 2:end);
-k = newtonInterpol(x1, y1)
-yfunc = @(x) k(1) + k(2) * (x-x1(1)) + k(3) * (x-x1(1)) * (x-x1(2));
-y237 = yfunc(2.37)
-errormargin = yfunc(2.37) - 1.83
 % Interpol margin of error
 x2 = xd(end - 3:end);
 y2 = yd(end - 3:end);
 k = newtonInterpol(x2, y2);
 yfunc2 = @(x) k(1) + k(2) * (x-x2(1)) + k(3) * (x-x2(1)) * (x-x2(2)) + k(4) * (x-x2(1)) * (x-x2(2)) * (x-x2(3));
 y237n2 = yfunc2(2.37);
-diff = y237-y237n2
-
+diff = y237-y237n2;
+root
 % ANSWERS:
 % root = 17.3108182 m/s
 % this gives us the final answer
 % "in order to hit the bull's-eye, the dart must be thrown with
 % 17.3108182 +- 5.93e-05 m/s."
 % maxDistancefrombullseye = 5.568718044468568e-07
-% It is within the bullseye because radius of bullseye is 6.37*e-03
+% It is within the bullseye because radius of bullseye is 6.35*e-03
 
