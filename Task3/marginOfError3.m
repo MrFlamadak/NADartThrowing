bd5153e689a3abe50c7dd64c18b06c9608c24060
Mostafa Aziz Zuher <thecool4a@gmail.com>
2023-06-06 12:21:04 +0200

    Put code of ninni file in marginOfError3.m

--- Task3/marginOfError3.m
+++ Task3/marginOfError3.m
@@ -62,3 +62,31 @@ nDistance = yFuncN(2.37) - 1.83
 
 % pmaxDistancefrombullseye = 5.51278e-06
 % nmaxDistancefrombullseye = 4.77311e-06
+
+
+% The correct way to calculate margin of error for nested methods
+% ***********************************************************************
+a = 3;
+m = 20e-3;  
+vGuess = 16;     
+y0 = 1.84;
+h = 1e-5;
+tol = 1e-4;
+%% original
+[rootO, secantError1] = secantV(y0, a, h, vGuess, m, tol)
+%% secant disrupted
+tol2 = 1e-5;
+[rootS, secantError1] = secantV(y0, a, h, vGuess, m, tol2)
+%% euler disrupted
+h2 = h/2;
+[rootE2, secantError1] = secantV(y0, a, h2, vGuess, m, tol)
+h4 = h/4
+
+[rootE4, secantError1] = secantV(y0, a, h4, vGuess, m, tol)
+skattning = (rootO-rootE2)/(rootE2-rootE4)
+
+EulerFel = abs(rootO - rootE2)
+TotalFel = abs(rootO - rootE2) + abs(rootO - rootS)
+sekantFel = abs(rootO - rootS)
+
+% ***********************************************************************
